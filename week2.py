1f31f24a260c6f41e0922230b1900110086b7e3c
Stephen Fickas <fickas@cs.uoregon.edu>
2017-12-19 08:39:23 -0800

    initial commit week2

---
+++ week2.py
@@ -0,0 +1,38 @@
+#Example use:
+#  titanic_table['pred_1'] = titanic_table.apply(lambda row: predictor_1(row), axis=1)
+#  titanic_table['pred_1_type'] = titanic_table.apply(lambda row: predictor_case(row, pred='pred_1', target='Survived'), axis=1)
+def predictor_case(row, pred, target):
+    actual = row[target]
+    prediction = row[pred]
+    if actual == 0 and prediction == 0:
+        case = 'true_negative'
+    elif actual == 1 and prediction == 1:
+        case = 'true_positive'
+    elif actual == 1 and prediction == 0:
+        case = 'false_negative'
+    else:
+        case = 'false_positive'
+    return case
+
+def f1(cases):
+	#the heart of the matrix
+	tp = cases['true_positive']
+	fn = cases['false_negative']
+	tn = cases['true_negative']
+	fp = cases['false_positive']
+	
+	#other measures we can derive
+	recall = 1.0*tp/(tp+fn)  # positive correct divided by total positive in the table
+	precision = 1.0*tp/(tp+fp) # positive correct divided by all positive predictions made
+	f1 = 2/(1/recall + 1/precision)
+	return f1
+
+def informedness(cases):
+    tp = cases['true_positive']
+    fn = cases['false_negative']
+    tn = cases['true_negative']
+    fp = cases['false_positive']
+    recall = 1.0*tp/(tp+fn)  # positive correct divided by total positive in the table
+    specificty = 1.0*tn/(tn+fp) # negative correct divided by total negative in the table
+    J = (recall + specificty) - 1
+    return J
